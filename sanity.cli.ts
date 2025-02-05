dfd06317a484e2fece0f44a85fc2fc01eb6e79aa
Kostadin Draganov <kostadindraganov@gmail.com>
2025-02-05 21:35:42 +0200

    fix sanity build

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,9 +1,13 @@
 import { defineCliConfig } from 'sanity/cli'
 import { projectId, dataset } from '@/sanity/lib/env'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineCliConfig({
 	api: {
 		projectId,
 		dataset,
 	},
+	vite: {
+    plugins: [tsconfigPaths()],
+  },
 })
