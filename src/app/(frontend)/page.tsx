c332faa838fabda5e448952a44694a1c56b30bc2
Kostadin Draganov <kostadindraganov@gmail.com>
2025-02-05 16:11:47 +0200

    test

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -16,7 +16,7 @@ export default  function Page() {
     
  
       <>
-       <Hero />
+       {/* <Hero /> */}
        <About />
        <Features />
        <FloatingImage />
@@ -26,13 +26,13 @@ export default  function Page() {
        <Labels />
        <Updates />
        <Contact /> 
-       <TypeShuffleComponent effect='fx1'>
+       {/* <TypeShuffleComponent effect='fx1'>
         <b>Aria McDonald</b>
         <b>Creative Web Developer</b>
         <b>5 years experience - HTML, CSS, JavaScript. Passion for creativity in the digital space. Problem solver. Hiker, guitar player, culinary enthusiast. Constantly seeking new challenges, growth opportunities.Bringing imaginative ideas to life. Skilled in modern web development frameworks such as React and Angular. Strong understanding of UI/UX design principles and ability to create visually appealing and usable websites.</b>
         <b>Eco Explorer, SkyBridge, SparkSail</b>
         <b>Best User Experience Design, Webby Awards 2021</b>
-       </TypeShuffleComponent>
+       </TypeShuffleComponent> */}
       </>
        
        
